700d9bc88b4c7da92f2d59555fe16e65aab4a8f5
Nexi <nexilios@gmail.com>
2021-07-17 12:43:46 +0700

    First Commit

---
+++ ESSAY - NO1/app/src/main/java/com/example/uasmoopessay/ESSAY_1A.java
@@ -0,0 +1,14 @@
+package com.example.uasmoopessay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ESSAY_1A extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.essay_1a);
+    }
+}
