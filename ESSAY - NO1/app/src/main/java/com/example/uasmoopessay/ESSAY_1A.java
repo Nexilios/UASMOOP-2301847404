396af47f292f68c55761f8d4bb049e1592a7e949
Nexi <nexilios@gmail.com>
2021-07-17 14:01:34 +0700

    2b

--- ESSAY - NO1/app/src/main/java/com/example/uasmoopessay/ESSAY_1A.java
+++ ESSAY - NO1/app/src/main/java/com/example/uasmoopessay/ESSAY_1A.java
@@ -2,7 +2,11 @@ package com.example.uasmoopessay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ESSAY_1A extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class ESSAY_1A extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.essay_1a);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello World!";
+        int duration = Toast.LENGTH_SHORT;
+        Button button = findViewById(R.id.HelloButton);
+        Toast toast = Toast.makeText(context, text, duration);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toast.show();
+            }
+        });
     }
 }
